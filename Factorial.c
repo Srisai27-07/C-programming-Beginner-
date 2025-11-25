c423ee8754920028d6c5ae02be43e8ea3f086b5a
Srisai Atipamulu <srisai.atipamulu25@pccoepune.org>
2025-11-25 17:45:04 +0530

    Implement recursive factorial function

--- Factorial.c
+++ Factorial.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 int main()
 {
@@ -10,4 +11,24 @@ int main()
     }
     printf("The factorial is: %d",fact);
      return 0;
+}
+*/
+
+
+#include<stdio.h>
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+    return 1;
+    else
+    return n * fact(n - 1);
+}
+
+int main()
+{
+    int num;
+    printf("Enter the number whose factorial you want: ");
+    scanf("%d",&num);
+    printf("The Factorial of %d is : %d ",num,fact(num));
+    return 0;
 }
