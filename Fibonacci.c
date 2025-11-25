b2cc2f8b2e026d1af7000b0814c45c86ad4c347c
Srisai Atipamulu <srisai.atipamulu25@pccoepune.org>
2025-11-25 18:15:18 +0530

    Fibonacci using Recursive Function.
    Implemented a recursive function to calculate Fibonacci numbers and updated the main function to print the Fibonacci series up to a user-defined number.

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 int main()
 {
@@ -14,4 +15,29 @@ int main()
         t2 = t3;
     }
      return 0;
+}
+*/
+
+#include<stdio.h>
+int fibb(int n)
+{
+    if(n == 0)
+    return 0;
+    else if(n == 1)
+    return 1;
+    else
+    return fibb(n - 1) + fibb(n - 2);
+}
+
+int main()
+{
+    int num,i;
+    printf("Enter the number till where you want the series: ");
+    scanf("%d",&num);
+    printf("The Fibonacci series is: \n");
+    for(i = 0;i < num;i++)
+    {
+        printf("%d \n",fibb(i));
+    }
+return 0;
 }
